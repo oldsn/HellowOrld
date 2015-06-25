8fda0aff443106bbaf4100dd7b3679f150c2a920
oldsn <oldsn.ck@msa.hinet.net>
2015-06-25 10:08:02 +0800

    test1111

--- app/src/main/java/idv/rai97/helloworld/MainActivity.java
+++ app/src/main/java/idv/rai97/helloworld/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
+//hellooooo
 public class MainActivity extends ActionBarActivity {
     //test qq
     @Override
